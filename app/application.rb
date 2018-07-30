0f456214ddbe7b7fb9efed1ca984b3ab96761c97
Adam Urbina <adamurbina@gmail.com>
2018-07-30 20:58:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,9 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+  elsif req.path.match(/add/)
+      item = req.params["q"]
+      resp.write handle_add(item)
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
